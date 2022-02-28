0b5ebe7c4c015d2c0095435709afaa9faf868119
Anderson <chltjdrhd777@naver.com>
2022-02-28 21:10:32 +0900

    Feat: 상세페이지 통합

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -3,6 +3,9 @@ import './css/index.css'
 import Header from './Header'
 import ProductInfo from './ProductInfo'
 import Image from './Image'
+import Social from './Social'
+import Contents from './Contents'
+import Comment from './Comment'
 
 export default function Index() {
   return (
@@ -10,6 +13,9 @@ export default function Index() {
       <Header />
       <ProductInfo />
       <Image />
+      <Social />
+      <Contents />
+      <Comment />
     </div>
   )
 }
