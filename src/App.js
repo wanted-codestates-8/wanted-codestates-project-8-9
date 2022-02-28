3cb31e6ec81162ac959c64275d883f68e8ba423b
starhn87 <starhn87@gmail.com>
2022-02-28 20:35:54 +0900

    Feat: 리덕스 초기 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
+import { useRef } from 'react'
+import { connect } from 'react-redux'
+import './App.css'
+import { add } from './modules/review'
 
-function App() {
+function App({ data, add }) {
+  let count = useRef(0)
+  const temp = {
+    userId: count.current++,
+  }
+
   return (
     <div className="App">
-      <div>
-        width
-      </div>
+      <button onClick={() => add(temp)}>Add</button>
+      {data &&
+        data.length > 0 &&
+        data.map((review, index) => <div key={index}>{review.userId}</div>)}
     </div>
-  );
+  )
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { data: state.review.data }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  add: (newReview) => {
+    dispatch(add(newReview))
+  },
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
