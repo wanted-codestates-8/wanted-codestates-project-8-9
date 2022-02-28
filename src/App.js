867b2e46f8c7f754bf8766e5861086596fce2450
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2022-02-28 20:04:10 +0900

    feat:리뷰쓰기 버튼 제작

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './App.css'
 import ReviewUpload from './pages/ReviewUpload'
 
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Link to="/review/upload" exact className="toReviewBtn">
+          리뷰 쓰기
+        </Link>
         <Routes>
           <Route path="/review/upload" exact element={<ReviewUpload />} />
         </Routes>
