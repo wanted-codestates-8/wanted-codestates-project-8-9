4293624d89f49178cba01449518806411e801373
guno517 <guno5170@gmail.com>
2022-03-02 17:25:30 +0900

    Feat: 상단 헤더 뒤로가기 기능 추가

--- src/components/Detail/Header.js
+++ src/components/Detail/Header.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './css/Header.css'
 import { AiOutlineArrowLeft, AiOutlineClose } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div className="header">
       <div className="goBack">
-        <AiOutlineArrowLeft size={30} />
+        <Link to="/" className="link">
+          <AiOutlineArrowLeft size={30} />
+        </Link>
       </div>
       <div className="title">리뷰 상세보기</div>
       <div className="close">
-        <AiOutlineClose size={30} />
+        <Link to="/" className="link">
+          <AiOutlineClose size={30} />
+        </Link>
       </div>
     </div>
   )
