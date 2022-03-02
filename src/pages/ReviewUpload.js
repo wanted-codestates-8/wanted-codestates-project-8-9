242047af13bd86d350a410a27472da2de37e5594
guno517 <guno5170@gmail.com>
2022-03-02 17:48:48 +0900

    Refactor: useRef 삭제

--- src/pages/ReviewUpload.js
+++ src/pages/ReviewUpload.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import Ratings from '../components/Ratings'
