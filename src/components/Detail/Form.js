da98e983e9b673e323c60a89caec7d39d1873851
starhn87 <starhn87@gmail.com>
2022-03-02 14:24:42 +0900

    feat: form 컴포넌트화

---
+++ src/components/Detail/Form.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './css/Comment.css'
+
+function Form({ value, onChange, onClick, index = -1, userId = '' }) {
+  return (
+    <form className="comment-input" onSubmit={(e) => e.preventDefault()}>
+      <input
+        type="text"
+        placeholder="tempUser (으)로 답글 달기"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <button className="btn submit" onClick={() => onClick(index, userId)}>
+        게시
+      </button>
+    </form>
+  )
+}
+
+export default Form
