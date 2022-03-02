8b19fed60fceca9fc14251db1b44b4091dc91c75
guno517 <guno5170@gmail.com>
2022-03-02 17:25:07 +0900

    Refactor: 안 쓰이는 컴포넌트 삭제

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -2,35 +2,26 @@ import React, { useState } from 'react'
 import './css/index.css'
 import Header from './Header'
 import ProductInfo from './ProductInfo'
-import Image from './Image'
 import Social from './Social'
 import Contents from './Contents'
 import Comment from './Comment'
 import Slick from './Slick'
 import ShareModal from './ShareModal'
-import { useSelector, useDispatch } from 'react-redux'
-import { useLocation } from 'react-router-dom'
 
 export default function Index() {
-  // dealing with share modal
   const [modalOpenState, setModalOpenState] = useState(false)
   function handleModalState() {
     setModalOpenState((prev) => !prev)
   }
-
-  // const param = useLocation()
 
   return (
     <div className="detailContainer">
       <Header />
       <ProductInfo />
       <Slick />
-
       <Social handleModalState={handleModalState} />
       <Contents />
       <Comment />
-
-      {/* ShareModal */}
       {modalOpenState && <ShareModal handleModalState={handleModalState} />}
     </div>
   )
